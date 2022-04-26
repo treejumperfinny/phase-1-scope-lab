59be49e791847a8bc584d7ab55863c709981fff1
Aya Ramirez <ayaramz@gmail.com>
2022-04-26 08:59:55 -0400

    commit tree b629999f2b6508b2706bb0fb28d09973ce765008

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer = 'not bob';
+const leastFavoriteCustomer = 'maybe bob';
+
+function upperCaseCustomerName(){
+   console.log(customerName.toUpperCase());
+   return customerName.toUpperCase;
+}
